2ae8480c49ff62e55cc1b9d8cd159bed7daea394
nguyenduong2612 <nguyenduong261298@gmail.com>
2020-03-05 16:31:21 +0700

    Run rubocop

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,38 +1,38 @@
-class PasswordResetsController < ApplicationController
-  before_action :get_user,   only: [:edit, :update]
-  before_action :valid_user, only: [:edit, :update]
-  before_action :check_expiration, only: [:edit, :update]
+# frozen_string_literal: true
 
-  def new
-  end
+class PasswordResetsController < ApplicationController
+  before_action :get_user,   only: %i[edit update]
+  before_action :valid_user, only: %i[edit update]
+  before_action :check_expiration, only: %i[edit update]
+
+  def new; end
 
   def create
     @user = User.find_by(email: params[:password_reset][:email].downcase)
     if @user
       @user.create_reset_digest
       @user.send_password_reset_email
-      flash[:info] = "Email sent with password reset instructions"
+      flash[:info] = 'Email sent with password reset instructions'
       redirect_to root_url
     else
-      flash.now[:danger] = "Email address not found"
+      flash.now[:danger] = 'Email address not found'
       render 'new'
     end
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
-    if params[:user][:password].empty?                 
+    if params[:user][:password].empty?
       @user.errors.add(:password, "can't be empty")
       render 'edit'
-    elsif @user.update_attributes(user_params)         
+    elsif @user.update_attributes(user_params)
       log_in @user
-      flash[:success] = "Password has been reset."
+      flash[:success] = 'Password has been reset.'
       redirect_to @user
     else
-      flash.now[:danger] = "Password invalid."
-      render 'edit'                                  
+      flash.now[:danger] = 'Password invalid.'
+      render 'edit'
     end
   end
 
@@ -47,14 +47,14 @@ class PasswordResetsController < ApplicationController
   end
 
   def valid_user
-    unless (@user && @user.activated? && @user.authenticated?(:reset, params[:id]))
+    unless @user&.activated? && @user&.authenticated?(:reset, params[:id])
       redirect_to root_url
     end
   end
 
   def check_expiration
     if @user.password_reset_expired?
-      flash[:danger] = "Password reset has expired."
+      flash[:danger] = 'Password reset has expired.'
       redirect_to new_password_reset_url
     end
   end
