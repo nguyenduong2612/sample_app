605335ae0e4d0704bb43ff91af75d74affa062f7
nguyenduong2612 <nguyenduong261298@gmail.com>
2020-03-05 12:53:49 +0700

    Fix updating users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      flash[:danger] = 'Profile updated'
+      flash[:success] = 'Profile updated'
       redirect_to @user
     else
       flash[:error] = 'Profile cannot update'
